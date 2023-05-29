10f28fd7ac20aaa7e57eaeb2634fcccc226e3a54
Azzam Adnan <azzamuadonang@gmail.com>
2023-05-30 02:22:13 +0700

    sequelize init pg

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
 // const port = process.env.PORT || 3000;
+const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize('postgres://default:BwfVY9tc7rKX@ep-tight-voice-970578-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb', {
+  dialectModule: require('pg')
+});
 
 app.use(cors());
 app.use(express.json());
