dd0530c72a4c3921c6e76737af36120f4c8bb1da
Azzam Adnan <azzamuadonang@gmail.com>
2023-05-30 00:12:04 +0700

    verchel init2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # zoofeed-CMS
 Project ZooFeed
+This project is a CMS project to manage users, animals, and several objects in the zoo-feed, as well as payment and ticket checking. The CMS consists of a server and a client. The backend server uses Express, while the client uses React. This project serves as the foundation for the mobile project of zoo-feed.
--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 
 app.use(cors());
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`App listen on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App listen on port ${port}`);
+// });
 
 module.exports = app;
