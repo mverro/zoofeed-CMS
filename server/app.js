8ccbcd9fac5cf1b91226dce192f51fa0feac4f26
Azzam Adnan <azzamuadonang@gmail.com>
2023-05-30 02:08:59 +0700

    package.json

--- server/app.js
+++ server/app.js
@@ -3,32 +3,11 @@ const express = require("express");
 const cors = require("cors");
 
 
-
 const app = express();
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
 // const port = process.env.PORT || 3000;
-
-const { Client } = require('pg');
-
-const client = new Client({
-  user: 'default',
-  host: 'ep-tight-voice-970578-pooler.ap-southeast-1.postgres.vercel-storage.com',
-  database: 'verceldb',
-  password: 'BwfVY9tc7rKX',
-  port: 5432,
-});
-
-client.connect()
-  .then(() => {
-    console.log('Koneksi berhasil!');
-  })
-  .catch((error) => {
-    console.error('Koneksi gagal:', error);
-  });
-
-
 
 
 app.use(cors());
