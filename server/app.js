8b1a9b2356f8d5bd52c541c69066aa83ba1666d4
Azzam Adnan <azzamuadonang@gmail.com>
2023-05-29 15:33:54 +0700

    push script

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
---
+++ server/scripts/initDB.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+npx sequelize-cli db:create && npx sequelize-cli db:migrate
